804e753a5da8eb694680427ee9fe416ceb03ff71
SAI TEJA <49040034+saiteja0211@users.noreply.github.com>
2019-12-06 11:24:34 +0530

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,30 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader,overrideDevServer,watchAll } = require('customize-cra');
+const reactAppRewireBuildDev = require('react-app-rewire-build-dev');
+
+
+const options = {
+	outputPath : "./build/innovation-hub-ui/",  /***** required *****/
+	basename : "."
+}
+
+//this is so that we can perform npm start and it will build
+//static files to our SS-Web folder to serve them.
+//with this, we will be able to save edits and the application will rebuild
+//with the static files in the right folder location
+
+const rewriteDevServer = (config, env) => {
+  return reactAppRewireBuildDev(config, env, options);
+}
 
 module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: true,
-  }),
-  addLessLoader({
-    javascriptEnabled: true,
-    modifyVars: { '@primary-color': '#25b864' },
-  }),
-);
+		rewriteDevServer,
+	    fixBabelImports('import', {
+	      libraryName: 'antd',
+	      libraryDirectory: 'es',
+	     style: true,
+	    }),
+	   addLessLoader({
+	     javascriptEnabled: true,
+	     modifyVars: { '@primary-color': '#c00' },
+	   }),
+	);
